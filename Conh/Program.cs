ef44c5ed66d5ee5cc8d23c53e58c75d1b2819b38
curso.ads <curso.ads@LAB06-71>
2022-08-15 22:05:01 -0300

    Adicionar arquivos de projeto.

---
+++ Conh/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conh
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //INSTANCIAÇAO DO OBJETO
+            Veiculo golf = new Veiculo();
+            Console.Write("Digite a marca: ");
+            string marca = Console.ReadLine();
+            Console.Write("Digite o modelo: ");
+            string modelo = Console.ReadLine();
+            Console.Write("Digite a placa: ");
+            string placa = Console.ReadLine();
+            Console.Write("Digite o ano de fabricaçao: ");
+            int ano = Convert.ToInt32(Console.ReadLine());
+            //objeto chamado metodo atribuir
+            golf.atribuir(marca,modelo,placa,ano);
+            Console.WriteLine(golf.retornar());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Conh/Veiculo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conh
+{
+    public class Veiculo
+    {
+        // declaracao de atributos
+        string marca;
+        string modelo;
+        string placa;
+        int ano;
+        // metodo para atribuir valores
+        public void atribuir(string ma,
+            string mo, string pl, int a)
+        {
+            this.ano = a;
+            this.marca = ma;
+            this.modelo = mo;
+            this.placa = pl;
+        }
+        public string retornar()
+        {
+            return "Marca =" + this.marca +
+                "\nModelo =" + this.modelo +
+                "\nAno de fabricaçao =" + this.ano +
+                "\nPlaca =" + this.placa;
+                }
+    }
+}
